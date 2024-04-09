ca507c4ffe7454849db74efd8a2fd4c5d377a8dd
Martina Baiardi <m.baiardi@unibo.it>
2024-04-09 18:15:23 +0200

    fix(deps): update to sdk level 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "it.unibo.jakta.examples.tictactoe"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "it.unibo.jakta.examples.tictactoe"
         minSdk = 25
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
